beb086bfc356ac8497ec85eb8b6e42464f26b2b6
Aman Kumar <amankumaroo784@gmail.com>
2024-09-23 18:11:54 +0530

    fixes for deployment

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static("dist"));
+
+app.get("*", (req, res) => {
+  res.send(path.join(path.resolve(), "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
+});
